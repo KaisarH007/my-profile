0a3d2b5599872cb109a4d940d70134e8fa8d45c3
KaisarH007 <kaisarh007@gamil.com>
2021-12-01 20:04:29 +0600

    modify

--- src/Pages/Home/AllProjects/AllProjects.js
+++ src/Pages/Home/AllProjects/AllProjects.js
@@ -12,7 +12,7 @@ const AllProjects = () => {
         <div className="d-flex align-items-center justify-content-center">
           <h3>MY PROJECTS</h3>
         </div>
-        <div data-aos="fade-right" className="row my-3">
+        <div data-aos="fade-right" className="row  my-3">
           <div className="col-md-6">
             <div>
               <img
@@ -28,11 +28,24 @@ const AllProjects = () => {
           <div className="col-md-6 d-flex align-items-center justify-content-center my-2">
             <div>
               <h2>About This Projects</h2>
+              <h5> Four Wings (MERN-stack, Niche Web App)</h5>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-                architecto! Distinctio veritatis autem libero, sint a, mollitia
-                molestias, magnam nisi molestiae nobis doloribus cum omnis!
-                Ducimus officiis quaerat consequatur dignissimos.
+                &#8226; Built a MERN Stack Niche web app also Implemented of
+                Admin and User Dashboard.
+              </p>
+              <p>
+                &#8226; Admin can manage all orders and products, add new
+                product, make another user admin, user can Manage her/his
+                orders,can give a review.
+              </p>
+              <p>
+                &#8226; Two Authentication Systems implemented with Firebase,
+                user can sign up with Google account and register/create a new
+                account by using email/password.
+              </p>
+              <p>
+                &#8226; Transferred data from Heroku via CRUD operation to UI.
+                This is full Mobile Responsive
               </p>
               <button className=" btn button-bg  text-white  me-3 ">
                 <a
@@ -55,11 +68,18 @@ const AllProjects = () => {
           <div className="col-md-6 d-flex align-items-center justify-content-center my-2">
             <div>
               <h2>About This Projects</h2>
+              <h5> Crunch Fitness (Gym Web App)</h5>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-                architecto! Distinctio veritatis autem libero, sint a, mollitia
-                molestias, magnam nisi molestiae nobis doloribus cum omnis!
-                Ducimus officiis quaerat consequatur dignissimos.
+                &#8226; This is a Gym web App. This app is full mobile
+                responsive.
+              </p>
+              <p>
+                &#8226; Implemented Authentication System with Firebase, user
+                can sign up with Google account.
+              </p>
+              <p>
+                &#8226; Service Details are protected, user need sign up for
+                seeing service details.
               </p>
               <button className=" btn button-bg text-white  me-3 ">
                 <a
@@ -102,11 +122,18 @@ const AllProjects = () => {
           <div className="col-md-6 d-flex align-items-center justify-content-center my-2">
             <div>
               <h2>About This Projects</h2>
+              <h2>Project Overview</h2>
+              <h5> Dreamy Tour (MERN-stack, Service Web App)</h5>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-                architecto! Distinctio veritatis autem libero, sint a, mollitia
-                molestias, magnam nisi molestiae nobis doloribus cum omnis!
-                Ducimus officiis quaerat consequatur dignissimos.
+                &#8226; This is a Tourism Services web app, user can book tour
+                service packages by giving some necessary information.
+              </p>
+              <p>
+                &#8226; Implemented protected route, Authentication System with
+                Firebase, user can sign up with Google account.
+              </p>
+              <p>
+                &#8226; Transferred data from Heroku via CRUD operation to UI.
               </p>
               <button className="btn button-bg  text-white  me-3 ">
                 <a
